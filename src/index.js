85dd1766a7dbfb251c9d9f6afb21f19f51ec38e1
ivan.demchenko.01 <ivan.demchenko.01@gmail.com>
2024-06-04 20:28:36 +0300

    +

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ axios.defaults.baseURL = 'https://my-json-server.typicode.com/ivandemchenko01/re
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <Router>
+    <Router basename='/react-s-shop'>
       <App/>
     </Router>
   // </React.StrictMode>
